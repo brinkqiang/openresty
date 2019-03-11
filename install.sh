c443f5532e90b72b82ddeb153ef3ec78387e3e50
brinkqiang <brink.qiang@gmail.com>
2019-03-11 17:55:57 +0800

    add

--- install.sh
+++ install.sh
@@ -9,4 +9,11 @@ sudo yum -y install yum-utils
 sudo yum -y install openresty-resty
 yum --disablerepo="*" --enablerepo="openresty" list available
 
+sudo groupadd nginx
+sudo useradd -g nginx -s /sbin/nologin -M nginx
+sudo openresty -s stop
+sudo openresty -t
+sudo openresty -c /usr/local/openresty/nginx/conf/nginx.conf
+ps axu | grep "nginx: master process" | grep -v grep | awk '{print $2 }'
+
 popd
